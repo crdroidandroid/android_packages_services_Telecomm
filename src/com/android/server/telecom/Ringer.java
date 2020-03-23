ab35f7bb7dab0cbd2125b6434a7a2a13c69ced03
Pranav Vashi <neobuddy89@gmail.com>
2020-03-23 06:07:18 +0530

    Telecomm: Fix few issues with Blink with call
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/server/telecom/Ringer.java
+++ src/com/android/server/telecom/Ringer.java
@@ -176,6 +176,8 @@ public class Ringer {
 
     private int torchMode;
 
+    private boolean mBlinkActive;
+
     /** Initializes the Ringer. */
     @VisibleForTesting
     public Ringer(
@@ -287,10 +289,12 @@ public class Ringer {
         torchMode = Settings.System.getIntForUser(mContext.getContentResolver(),
                  Settings.System.FLASHLIGHT_ON_CALL, 0, UserHandle.USER_CURRENT);
 
-        boolean shouldFlash = (torchMode == 1 && !dndMode) ||
-                              (torchMode == 2 && dndMode)  ||
+        boolean shouldFlash = (torchMode == 1 && isRingerAudible) ||
+                              (torchMode == 2 && !isRingerAudible)  ||
                                torchMode == 3;
-        if (shouldFlash) {
+
+        if (shouldFlash && !mBlinkActive) {
+            mBlinkActive = true;
             blinkFlashlight();
         }
             return shouldAcquireAudioFocus;
@@ -448,6 +452,8 @@ public class Ringer {
     }
 
     public void startCallWaiting(Call call, String reason) {
+        mBlinkActive = false;
+
         if (mSystemSettingsUtil.isTheaterModeOn(mContext)) {
             return;
         }
@@ -486,9 +492,9 @@ public class Ringer {
             mRingingCall = null;
         }
 
+        mBlinkActive = false;
         mRingtonePlayer.stop();
 
-        torchToggler.stop();
         // If we haven't started vibrating because we were waiting for the haptics info, cancel
         // it and don't vibrate at all.
         if (mVibrateFuture != null) {
@@ -585,7 +591,6 @@ public class Ringer {
 
     private class TorchToggler extends AsyncTask {
 
-        private boolean shouldStop = false;
         private CameraManager cameraManager;
         private int duration = 400;
         private boolean hasFlash = true;
@@ -601,16 +606,12 @@ public class Ringer {
             hasFlash = context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
         }
 
-        void stop() {
-            shouldStop = true;
-        }
-
         @Override
         protected Object doInBackground(Object[] objects) {
             if (hasFlash) {
                 try {
                     String cameraId = cameraManager.getCameraIdList()[0];
-                    while (!shouldStop) {
+                    while (mBlinkActive) {
                         cameraManager.setTorchMode(cameraId, true);
                         Thread.sleep(duration);
 

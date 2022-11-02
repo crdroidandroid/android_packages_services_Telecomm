52cb70dec290680b995e05ac26f6f55dbf8d9099
Ido Ben-Hur <idoybh2@gmail.com>
2022-11-02 03:10:32 +0200

    Ringer: There should really not be an observer here
    It'd never unregister and cause a memory leak
    
    Change-Id: I2c71bd81cd2f38298a398a2a31879d2d490b5c5b
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/server/telecom/Ringer.java
+++ src/com/android/server/telecom/Ringer.java
@@ -22,7 +22,6 @@ import android.app.Person;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.pm.PackageManager;
-import android.database.ContentObserver;
 import android.hardware.camera2.CameraManager;
 import android.provider.Settings;
 import android.telecom.Log;
@@ -189,9 +188,6 @@ public class Ringer {
     };
 
     private boolean mUseSimplePattern;
-    private int mVibrationPattern;
-    private SettingsObserver mSettingObserver;
-    private final Handler mH = new Handler();
 
     /**
      * Indicates that vibration should be repeated at element 5 in the {@link #PULSE_AMPLITUDE} and
@@ -278,8 +274,6 @@ public class Ringer {
         mInCallController = inCallController;
         mVibrationEffectProxy = vibrationEffectProxy;
         mUseSimplePattern = mContext.getResources().getBoolean(R.bool.use_simple_vibration_pattern);
-        mVibrationPattern = Settings.System.getIntForUser(mContext.getContentResolver(),
-            Settings.System.RINGTONE_VIBRATION_PATTERN, 0, UserHandle.USER_CURRENT);
         torchToggler = new TorchToggler(context);
 
         if (mContext.getResources().getBoolean(R.bool.use_simple_vibration_pattern)) {
@@ -290,18 +284,8 @@ public class Ringer {
                     PULSE_AMPLITUDE, REPEAT_VIBRATION_AT);
         }
 
-        mSettingObserver = new SettingsObserver(mH);
-        mContext.getContentResolver().registerContentObserver(
-            Settings.System.getUriFor(Settings.System.RINGTONE_VIBRATION_PATTERN),
-            true, mSettingObserver, UserHandle.USER_CURRENT);
-        mContext.getContentResolver().registerContentObserver(
-            Settings.System.getUriFor(Settings.System.CUSTOM_RINGTONE_VIBRATION_PATTERN),
-            true, mSettingObserver, UserHandle.USER_CURRENT);
-
         mIsHapticPlaybackSupportedByDevice =
                 mSystemSettingsUtil.isHapticPlaybackSupported(mContext);
-
-        updateVibrationPattern();
     }
 
     @VisibleForTesting
@@ -393,6 +377,7 @@ public class Ringer {
         stopCallWaiting();
 
         VibrationEffect effect;
+        updateVibrationPattern();
         CompletableFuture<Boolean> hapticsFuture = null;
         // Determine if the settings and DND mode indicate that the vibrator can be used right now.
         boolean isVibratorEnabled = isVibratorEnabled(mContext, foregroundCall);
@@ -717,10 +702,10 @@ public class Ringer {
     }
 
     private void updateVibrationPattern() {
-        mVibrationPattern = Settings.System.getIntForUser(mContext.getContentResolver(),
-            Settings.System.RINGTONE_VIBRATION_PATTERN, 0, UserHandle.USER_CURRENT);
+        final int pattern = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.RINGTONE_VIBRATION_PATTERN, 0, UserHandle.USER_CURRENT);
         if (mUseSimplePattern) {
-            switch (mVibrationPattern) {
+            switch (pattern) {
                 case 1:
                     mDefaultVibrationEffect = mVibrationEffectProxy.createWaveform(DZZZ_DA_VIBRATION_PATTERN,
                         FIVE_ELEMENTS_VIBRATION_AMPLITUDE, REPEAT_SIMPLE_VIBRATION_AT);
@@ -771,17 +756,6 @@ public class Ringer {
         } else {
             mDefaultVibrationEffect = mVibrationEffectProxy.createWaveform(PULSE_PATTERN,
                     PULSE_AMPLITUDE, REPEAT_VIBRATION_AT);
-        }
-    }
-
-    private final class SettingsObserver extends ContentObserver {
-        public SettingsObserver(Handler handler) {
-            super(handler);
-        }
-
-        @Override
-        public void onChange(boolean SelfChange) {
-            updateVibrationPattern();
         }
     }
 }

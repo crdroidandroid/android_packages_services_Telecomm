ac1a73e6bc8c31d67280dc7307864f2f325e4e7d
pengfeix <quic_pengfeix@quicinc.com>
2025-03-06 05:49:05 +0530

    Fix NullPointerException issue due to threads race
    - In current implementation, the CountDownLatch variable
      is initialized improperly between two threads.
      The initialization and use of the CountDownLatch variable
      are in different threads. That will cause
      NullPointerException exception due to threads race.
      To fix it, initializing the CountDownLatch variable
      in one thread before another thread which using
      the CountDownLatch variable is started.
    
    Bug: 400612242
    Test: manual
    Flag: NONE bugfix
    Google: 3542210
    Change-Id: Id3dac00c5edac683d0356c09907ef6efb3a228a8

--- src/com/android/server/telecom/Ringer.java
+++ src/com/android/server/telecom/Ringer.java
@@ -380,6 +380,8 @@ public class Ringer {
                 return false;
             }
 
+            mAttributesLatch = new CountDownLatch(1);
+
             // Use completable future to establish a timeout, not intent to make these work outside
             // the main thread asynchronously
             // TODO: moving these RingerAttributes calculation out of Telecom lock to avoid blocking
@@ -389,7 +391,6 @@ public class Ringer {
 
             RingerAttributes attributes = null;
             try {
-                mAttributesLatch = new CountDownLatch(1);
                 attributes = ringerAttributesFuture.get(
                         RINGER_ATTRIBUTES_TIMEOUT, TimeUnit.MILLISECONDS);
             } catch (ExecutionException | InterruptedException | TimeoutException e) {
@@ -959,7 +960,9 @@ public class Ringer {
             call.setUserMissed(USER_MISSED_DND_MODE);
         }
 
-        mAttributesLatch.countDown();
+        if (mAttributesLatch != null) {
+            mAttributesLatch.countDown();
+        }
         return builder.setEndEarly(endEarly)
                 .setLetDialerHandleRinging(letDialerHandleRinging)
                 .setAcquireAudioFocus(shouldAcquireAudioFocus)
